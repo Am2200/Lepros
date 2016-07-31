b9285fbfe808d50be84cd95bd2da666240ff0008
Am2200 <am.200@rambler.ru>
2016-07-31 13:06:10 +0300

    Good version with Active Record

--- app.rb
+++ app.rb
@@ -15,8 +15,7 @@ class Comment < ActiveRecord::Base
 end
 
 get '/' do
-  @posts = Post.all
-	erb :index
+  erb 'Hello World!'
 end
 
 get '/new' do
@@ -26,24 +25,24 @@ end
 post '/new' do
   newPost = Post.new params[:post]
   newPost.save
+  redirect to ('/posts')
+end
+
+get '/posts' do
+  @posts = Post.all
   erb :index
 end
 
 get '/details/:post_id' do
   post_id = params[:post_id]
-	results = @db.execute 'SELECT * FROM Posts where id = ?',[post_id]
-  @row = results[0]
-
-  @comments = @db.execute 'SELECT * FROM Comments WHERE post_id = ? ORDER BY id', [post_id]
-
+  @post = Post.find post_id
+  @comments = Comment.where("idPost = ?", post_id)
+  #@lastComment = Comment.last
   erb :details
 end
 
 post '/details/:post_id' do
-  post_id = params[:post_id]
-  content = params[:comment]
-
-  @db.execute 'INSERT INTO Comments (content, created_date, post_id) values (?, datetime(), ?)', [content, post_id]
-
-	redirect to ('/details/' + post_id)
+  newComment = Comment.new params[:comment]
+  newComment.save
+	redirect to ('/details/' + params[:post_id])
 end
